7a648d1ef8621e8e8ce9076a2c4624d81196517a
Michael Connell <michaelconnell@Michaels-M1-MacBook-Pro.local>
2024-03-03 14:30:32 -0800

    add incremental models where possible

--- models/staging/stg_order.sql
+++ models/staging/stg_order.sql
@@ -1,8 +1,19 @@
 -- depends_on: {{ ref('stg_sales_data') }}
 
+{{
+    config(
+        schema='staging',
+        materialized='incremental'
+    )
+}}
+
 select distinct
     order_number,
     order_line_number,
     order_date,
     status
 from {{ source('salesforce', 'stg_sales_data') }}
+
+{% if is_incremental() %}
+    where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
--- models/staging/stg_sales_data.sql
+++ models/staging/stg_sales_data.sql
@@ -1,7 +1,10 @@
-{{ config(schema='staging', materialized='table') }}
+{{ config(schema='staging', materialized='incremental') }}
 
 select
     {{ dbt_utils.generate_surrogate_key(['order_number','order_line_number']) }} id,
     *,
     {{ current_timestamp() }} data_loaded_at
 from {{ ref('sales_data_raw') }}
+{% if is_incremental() %}
+  where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
